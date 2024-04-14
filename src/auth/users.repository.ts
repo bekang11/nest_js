cc287014453c4aefea883b1ba042efe438b587b5
Manh Hoang <manhhoang@192.168.2.63>
2024-04-14 10:12:10 -0700

    User

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,29 @@
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+@Injectable() // here
+export class UsersRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === '23505') {
+        throw new ConflictException('Tai Khoan Ton Tai');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
